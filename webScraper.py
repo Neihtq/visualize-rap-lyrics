d007fc700216fa605bb96060410c669ca95d602b
neihtfool2 <q.thien.nguyen@outlook.de>
2019-04-23 16:07:59 +0200

    completed scraping overview pages

--- webScraper.py
+++ webScraper.py
@@ -1,17 +1,22 @@
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 
+URL = ['all.html', 'all_two.html', 'all_three.html', 'all_four.html', 'all_five.html']
+
 def get_html(url):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
     return soup
 
 def get_rappers(soup):
-    artists = soup.find_all('pre').find_all('a')
-    dict_set = set()
+    ''' store name and url of artis in a dicitonary
+    returns list of dictionaries'''
+    pre = soup.find('pre')
+    artists = pre.find_all('a', href=True)
+    dict_list = []
     for a in artists:
         stored_obj = {}
-        stored_obj["name"] = a.text
-        stored_obj["href"] = a['href']
-        dict_lists.add(stored_obj)
-    return dict_set
+        stored_obj['name'] = a.text
+        stored_obj['href'] = a['href']
+        dict_list.append(stored_obj)
+    return dict_list
