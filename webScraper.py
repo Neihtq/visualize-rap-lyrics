62233c7e50ec33491bd5f6d54c3f6ad840380496
neihtfool2 <q.thien.nguyen@outlook.de>
2019-05-08 11:34:09 +0200

    Added crawler for pages from ftp server

--- webScraper.py
+++ webScraper.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 
 URL = ['all.html', 'all_two.html', 'all_three.html', 'all_four.html', 'all_five.html']
 
+
 def get_html(url):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
     return soup
+
 
 def get_rappers(soup):
     ''' store name and url of artis in a dicitonary
@@ -20,3 +22,35 @@ def get_rappers(soup):
         stored_obj['href'] = a['href']
         dict_list.append(stored_obj)
     return dict_list
+
+def get_dir_ftp(href):
+    soup = get_html(href)
+    links = soup.find_all('a', text=True)
+    dict_list = []
+    for a in links[5:]:
+        stored_obj = {}
+        stored_obj['album'] = a.text
+        stored_obj['href'] = a['href']
+        dict_list.append(stored_obj)
+    return dict_list
+
+def get_track_dir_ftp(href):
+    soup = get_html(href)
+    links = soup.find_all('a', text=True)
+    dict_list = []
+    for a in links[5:]:
+        stored_obj = {}
+        stored_obj['album'] = a.text
+        stored_obj['href'] = a['href']
+        dict_list.append(stored_obj)
+    return dict_list
+
+def get_lyrics_raw(href):
+    soup = get_html(href)
+    text = soup.text.splitlines()[5:]
+    lyrics = ""
+    for line in text:
+        lyrics += line
+        if text.index(line) != len(text)-1:
+            lyrics += "\n"
+    return lyrics
